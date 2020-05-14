5e257209a0e4a77e5000ac2c5541d1bcf5364ace
panama <hmspanama@hmspanama.uoregon.edu>
2020-05-13 17:02:17 -0700

    new example added

---
+++ examples/calc_vols.py
@@ -0,0 +1,36 @@
+import sys
+sys.path.insert(0,'CHANGE/THIS/TO/THE/LOCATION/OF/STREUSEL/ON/YOUR/COMPUTER/')
+from gaussian_cube import *
+from tqdm import tqdm
+import pandas as pd
+import glob
+import os
+
+df = pd.DataFrame()
+
+for mpath in glob.glob('/PATH/TO/CUBE/FILES/*.cube'):
+  el = mpath.split('/')
+  molecule = el[len(el)-2]
+  print(molecule)
+  m = el[len(el)-1]
+  print(m)
+ 
+  mol = Molecule(mpath)
+  vecs = mol.vecs
+  ngs = mol.ngs
+  mol.get_efield()
+  mol.sample_efield()
+  streusel = mol.vol
+  print('volume is -------')
+  print(streusel) 
+  row = pd.DataFrame(data=[[molecule, streusel]])
+  tempdf = pd.concat([df, row], axis=0)
+  df = pd.DataFrame(data=tempdf)
+  
+  print('--------------------------------------------------------------------------')
+
+df.columns = ['molecule','vol']
+print(df)
+df.to_csv('final_volumes.csv', index=False)
+
+
