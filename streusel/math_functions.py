80e27d00a045f0abf30187c390f58815a6be6514
austin-mroz <austin.mroz@gmail.com>
2022-05-12 23:30:12 +0100

    Improve performance of electric field calculation (#1)
    Significant decrease in time it takes to calculate the electric field
    magnitude by moving away from nested for loops and taking advantage
    of numpy.
    
    Co-authored-by: Lukas Turcani <lukasturcani@mailbox.org>

--- streusel/math_functions.py
+++ streusel/math_functions.py
@@ -18,10 +18,10 @@ Args:
 	gx/y/z : fields in x y and z directions 2D array
 Returns:
 	grad_mag : gradient of fields at each point"""
-	grad_mag = gx
-	for i in range(gx.shape[0]):
-		for j in range(gy.shape[1]):
-			for k in range(gz.shape[2]):
-				grad_mag[i,j,k] = np.sqrt(gx[i,j,k]**2+gy[i,j,k]**2+gz[i,j,k]**2)
 
-	return grad_mag
+        out = np.square(gx)
+        out += np.square(gy)
+        out += np.square(gz)
+        
+        return np.sqrt(out, out=out)
+
