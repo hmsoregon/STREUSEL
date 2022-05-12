39113d32af9bd8aac0cfbb538adea0daf56144a0
austin-mroz <austin.mroz@gmail.com>
2022-05-12 23:51:18 +0100

    Fix tabs in `math_functions.py`
    Converted tabs to spaces in `math_functions.py'
    Also, bumped version
    
    Co-authored-by: Lukas Turcani <lukasturcani@mailbox.org>

--- streusel/math_functions.py
+++ streusel/math_functions.py
@@ -1,27 +1,31 @@
 import numpy as np 
 
 def c2f(x):
-	""" Converts 'x' to a float """
-	try:
-		x = int(x)
-		return x
-	except ValueError:
-		try:
-			x = float(x)
-			return x
-		except ValueError:
-			return x
+    """ 
+    Converts 'x' to a float 
+    """
+    try:
+        x = int(x)
+        return x
+    except ValueError:
+        try:
+            x = float(x)
+            return x
+        except ValueError:
+            return x
 
 def grad_magnitude(gx, gy, gz):
-	"""Converts the separate gradient magnitudes to a single magnitude
-Args:
-	gx/y/z : fields in x y and z directions 2D array
-Returns:
-	grad_mag : gradient of fields at each point"""
+    """
+    Converts the separate gradient magnitudes to a single magnitude
+    Args:
+        gx/y/z : fields in x y and z directions 2D array
+    Returns:
+        grad_mag : gradient of fields at each point
+    """
 
-        out = np.square(gx)
-        out += np.square(gy)
-        out += np.square(gz)
+    out = np.square(gx)
+    out += np.square(gy)
+    out += np.square(gz)
         
-        return np.sqrt(out, out=out)
+    return np.sqrt(out, out=out)
 
